1a40265161371db68f87ac14660d08d03e31fa1f
Deepak Khatri <deepaklorkhatri7@gmail.com>
2020-08-28 23:00:03 +0530

    fix burger and update collapsable description

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,19 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50 ) {
+      $('.scrolltop:hidden').stop(true, true).fadeIn();
+  } else {
+      $('.scrolltop').stop(true, true).fadeOut();
+  }
+});
+$(function(){
+  $(".scroll").click(function(){
+    $("html,body").animate({
+      scrollTop:$(".masthead").offset().top
+    },"1000");
+    return false
+  })
+})
